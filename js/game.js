b8f73f6ed3b7b79212c424214161233c7484a9a1
yonatan pozner <106734110+yonatanpozner@users.noreply.github.com>
2023-05-31 21:41:01 +0300

    basic game

--- js/game.js
+++ js/game.js
@@ -5,15 +5,77 @@ var gGame
 
 function onInit(){
     gGame = {
-        currLevel: LEVELS[0]
+        isOn : true,
+        currLevel: LEVELS[1]
     }
 
     createBoard()
-    console.log('gBoard: ', gBoard)
     addMines()
     renderBoard()
 }
 
 function onCellClicked(elCell, i, j){
+    var cell = gBoard[i][j]
 
+    if(!gGame.isOn) return
+    if(cell.isShown) return
+    if(cell.isMarked) return
+
+    cell.isShown = true
+       
+    if(cell.isMine) gameOver(0)
+    if(checkVictory()) gameOver(1)
+    expandShown(elCell,i,j)
+    renderBoard()
+}
+
+function onCellMarked(elCell,i,j,event){
+    event.preventDefault()
+    if(elCell.classList.contains('cover') && gGame.isOn){
+        gBoard[i][j].isMarked = !gBoard[i][j].isMarked
+        if(checkVictory()){
+            gameOver(1)
+        }
+        renderBoard()
+    }
+}
+
+function expandShown(elCell, rowIdx, colIdx){
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= gBoard.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === rowIdx && j === colIdx) continue
+            if (j < 0 || j >= gBoard[0].length) continue
+            var currCell = gBoard[i][j]
+            if (!currCell.isMine) currCell.isShown = true
+        }
+    }
+}
+
+function checkVictory(){
+    var count = 0
+    for (var i = 0; i < gGame.currLevel.size; ++i) {
+        for (var j = 0; j < gGame.currLevel.size; ++j) {
+            var cell = gBoard[i][j] 
+            if(isGoodCell(cell)){
+                count++
+            }
+        }
+    }
+    // console.log('count: ', count)
+    return count === gGame.currLevel.size ** 2
+}
+
+function isGoodCell(cell){
+    return cell.isShown || cell.isMarked && cell.isMine
+}
+
+function gameOver(isVictory){
+    gGame.isOn = false
+    if(isVictory){
+        console.log('victory!')
+    } else {
+        showAllCells()
+        console.log('game over')
+    }
 }
