bf5659772fe62878ccaf40c3a197e6f187b88fea
yonatan pozner <106734110+yonatanpozner@users.noreply.github.com>
2023-06-01 12:00:32 +0300

    including additional tasks

---
+++ js/emojis.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const MINE = `💣`
+const FLAG = '\u{1F6A9}'
+const HAPPY = '😄'
+const SAD = '😵'
+const WIN = '😎'
+const EXPLOSION = '💥'
--- js/mines.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-const MINE = `💣`
-const FLAG = '\u{1F6A9}'
