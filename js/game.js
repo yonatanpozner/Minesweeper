ac9c21357ded0a7a910bf0b88c02a5fd422e1ea0
yonatan pozner <106734110+yonatanpozner@users.noreply.github.com>
2023-06-01 12:01:30 +0300

    including additional tasks

--- js/game.js
+++ js/game.js
@@ -1,81 +1,134 @@
 'use strict'
 
-const LEVELS = [{size: 4, minesCount: 2}, {size: 6, minesCount: 14}, {size: 8, minesCount: 32}]
+const LEVELS = [{ size: 4, minesCount: 2 }, { size: 6, minesCount: 14 }, { size: 8, minesCount: 32 }]
+const LIVES = 2
+const VICTORY = 1
+const LOSS = 0
 var gGame
 
-function onInit(){
+function onInit(level) {
     gGame = {
-        isOn : true,
-        currLevel: LEVELS[1]
+        isOn: true,
+        isFirstClick: true,
+        currLevelIdx : level,
+        currLevel: LEVELS[level],
+        lives: LIVES,
+        smiley: HAPPY
     }
 
     createBoard()
     addMines()
+    setMinesNegsCount()
     renderBoard()
+    renderLives()
+    renderSmiley()
 }
 
-function onCellClicked(elCell, i, j){
+function onCellClicked(i, j) {
     var cell = gBoard[i][j]
 
-    if(!gGame.isOn) return
-    if(cell.isShown) return
-    if(cell.isMarked) return
+    if (!gGame.isOn) return
+    if (cell.isShown) return
+    if (cell.isMarked) return
+    if (gGame.isFirstClick && cell.isMine) {
+        moveMine(i, j)
+    }
+    if (gGame.isFirstClick) gGame.isFirstClick = false
 
     cell.isShown = true
-       
-    if(cell.isMine) gameOver(0)
-    if(checkVictory()) gameOver(1)
-    expandShown(elCell,i,j)
+
+    if (cell.isMine) {
+        if (!gGame.lives) {
+            gameOver(LOSS)
+        } else {
+            updateLives()
+        }
+    } else {
+        expandShown(i, j)
+    }
+    if (checkVictory() && gGame.isOn) gameOver(VICTORY)
     renderBoard()
 }
 
-function onCellMarked(elCell,i,j,event){
+function onCellMarked(elCell, i, j, event) {
     event.preventDefault()
-    if(elCell.classList.contains('cover') && gGame.isOn){
+    if (elCell.classList.contains('cover') && gGame.isOn) {
         gBoard[i][j].isMarked = !gBoard[i][j].isMarked
-        if(checkVictory()){
-            gameOver(1)
+        if (checkVictory()) {
+            gameOver(VICTORY)
         }
         renderBoard()
     }
 }
 
-function expandShown(elCell, rowIdx, colIdx){
+function onLevelClicked(level,idx){
+
+    var levelBtns = document.querySelectorAll('.level-btn')
+    for(var i = 0; i < levelBtns.length; ++i){
+        levelBtns[i].classList.remove('curr-level-btn')
+        console.log('levelBtns[i]: ', levelBtns[i])
+    }
+    level.classList.add('curr-level-btn')
+    onInit(idx)
+}
+
+function expandShown(rowIdx, colIdx) {
     for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
         if (i < 0 || i >= gBoard.length) continue
         for (var j = colIdx - 1; j <= colIdx + 1; j++) {
             if (i === rowIdx && j === colIdx) continue
             if (j < 0 || j >= gBoard[0].length) continue
             var currCell = gBoard[i][j]
-            if (!currCell.isMine) currCell.isShown = true
+            if (!currCell.isMine && !currCell.isShown) {
+                currCell.isShown = true
+                if (!currCell.minesAroundCount) {
+                    expandShown(i, j)
+                }
+            }
         }
     }
 }
 
-function checkVictory(){
+function checkVictory() {
     var count = 0
     for (var i = 0; i < gGame.currLevel.size; ++i) {
         for (var j = 0; j < gGame.currLevel.size; ++j) {
-            var cell = gBoard[i][j] 
-            if(isGoodCell(cell)){
+            var cell = gBoard[i][j]
+            if (cell.isShown || cell.isMarked && cell.isMine) {
                 count++
             }
         }
     }
-    // console.log('count: ', count)
     return count === gGame.currLevel.size ** 2
 }
 
-function isGoodCell(cell){
-    return cell.isShown || cell.isMarked && cell.isMine
-}
-
-function gameOver(isVictory){
+function gameOver(isVictory) {
     gGame.isOn = false
-    if(isVictory){
+    if (isVictory) {
+        gGame.smiley = WIN
+        renderSmiley()
         console.log('victory!')
     } else {
+        gGame.smiley = SAD
+        renderSmiley()
         showAllCells()
         console.log('game over')
     }
+}
+
+function onResetGame(){
+    onInit(gGame.currLevelIdx)
+}
+
+function updateLives() {
+    --gGame.lives
+    document.querySelector('.lives span').innerHTML = gGame.lives
+}
+
+function renderSmiley(){
+    document.querySelector('.smiley').innerHTML = gGame.smiley
+}
+
+function renderLives() {
+    document.querySelector('.lives').innerHTML = gGame.lives
 }
