9e7ae6ec8a737da8958f3dc436298c8061d5386f
yonatan pozner <106734110+yonatanpozner@users.noreply.github.com>
2023-06-01 11:59:32 +0300

    including additional tasks

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,31 @@
 'use strict'
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
 }
+
+function randomIntArrayInRange(length) {
+    var array = Array.from({length: length}, (_, index) => index)
+    shuffle(array)
+    return array
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
